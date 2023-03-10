ca863e5e2317b54e4c0e34557428d97dc6286e0f
LucianoBrumer <lucianobrumer5@gmail.com>
2023-03-09 22:07:00 -0300

    first commit

---
+++ Fullscreen/fullpage.js
@@ -0,0 +1,89 @@
+let keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+    e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+let supportsPassive = false;
+try {
+    window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+        get: function () { supportsPassive = true; }
+    }));
+} catch(e) {}
+
+let wheelOpt = supportsPassive ? { passive: false } : false;
+let wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+function disableScroll() {
+    window.addEventListener('DOMMouseScroll', preventDefault, false);
+    window.addEventListener(wheelEvent, preventDefault, wheelOpt);
+    window.addEventListener('touchmove', preventDefault, wheelOpt);
+    window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+function enableScroll() {
+    window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+    window.removeEventListener('touchmove', preventDefault, wheelOpt);
+    window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+let pages = document.querySelectorAll('.page')
+
+let pagesCount = pages.length
+
+let currentPage = 0
+
+let keyUps = [87, 38]
+let keyDowns = [83, 40, 32]
+
+function scrollUp(){
+    if(window.scrollY === pages[currentPage].getBoundingClientRect().top + window.scrollY){
+        if(currentPage > 0){
+            currentPage -= 1
+            pages[currentPage].scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
+        }
+    }else{
+        pages[currentPage].scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
+    }
+}
+
+function scrollDown(){
+    if(window.scrollY === pages[currentPage].getBoundingClientRect().top + window.scrollY){
+        currentPage += 1
+        if(currentPage >= pagesCount) currentPage = 0
+        pages[currentPage].scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
+    }else{
+        pages[currentPage].scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
+    }
+}
+
+if(Object.values(document.body.classList).includes('fullpage')){
+    disableScroll()
+
+    window.addEventListener('keydown', e => {
+        if(keyUps.includes(e.keyCode) && currentPage > 0) scrollUp()
+        if(keyDowns.includes(e.keyCode)) scrollDown()
+    })
+
+    window.addEventListener('DOMMouseScroll', e => {
+        e.preventDefault()
+        console.log('DOMMouseScroll');
+    }, false);
+
+    window.addEventListener(wheelEvent, e => {
+        if(e.deltaY > 0) scrollDown()
+        else scrollUp()
+    }, wheelOpt);
+
+    window.addEventListener('touchmove', e => {
+        console.log('touchmove');
+    }, wheelOpt);
+}
---
+++ Slider/slider.js
@@ -0,0 +1,204 @@
+const sliders = document.querySelectorAll('.slider')
+
+document.head.innerHTML += `
+    <style>
+        .slider {
+            position: relative;
+        }
+
+        .slider-item {
+            position: absolute;
+            left: 0;
+            top: 0;
+        }
+    </style>
+`
+
+sliders.forEach(slider => {
+    slider.index = Number(slider.getAttribute('index')) || 0
+    slider.lastIndex = slider.index
+
+    slider.style.aspectRatio = slider.getAttribute('aspect-ration') || '16/9'
+    slider.style.overflow = slider.getAttribute('overflow') || 'hidden'
+
+    slider.animation = slider.getAttribute('animation') || 'slide'
+    slider.animationDuration = Number(slider.getAttribute('animation-duration')) || 1000
+    slider.animationEasing = slider.getAttribute('animation-easing') || 'ease-in-out'
+    slider.animationTimeout = null
+
+    slider.sliderItems = slider.querySelectorAll('.slider-item')
+    slider.sliderItems.forEach((sliderItem, index) => {
+        sliderItem.style.width = getComputedStyle(slider).width
+        if(slider.index != index){
+            switch(slider.animation){
+                case 'slide':
+                    sliderItem.style.left = `${getComputedStyle(slider).width}`
+                    break
+                case 'fade':
+                    sliderItem.style.opacity = `0`
+                    break
+                case 'pull':
+                    sliderItem.style.zIndex = 0
+                    break
+                case 'push':
+                    sliderItem.style.left = `${getComputedStyle(slider).width}`
+                    break
+                case 'scale':
+                    break
+            }
+        }else{
+            switch(slider.animation){
+                case 'pull':
+                    sliderItem.style.zIndex = 10
+                    break
+                case 'push':
+                    break
+                case 'scale':
+                    break
+            }
+        }
+    })
+
+
+    slider.next = () => {
+        if(slider.animationTimeout == null){
+            slider.lastIndex = slider.index
+            slider.index += 1
+            if(slider.index >= slider.sliderItems.length) slider.index = 0
+
+            switch(slider.animation){
+                case 'slide':
+                    slider.lastIndexAnimation = [
+                        {left: `0`},
+                        {left: `-${getComputedStyle(slider).width}`}
+                    ]
+                    slider.indexAnimation = [
+                        {left: `${getComputedStyle(slider).width}`},
+                        {left: '0'}
+                    ]
+                    break
+                case 'fade':
+                    slider.lastIndexAnimation = [
+                        {opacity: '1'},
+                        {opacity: '0'}
+                    ]
+                    slider.indexAnimation = [
+                        {opacity: '0'},
+                        {opacity: '1'}
+                    ]
+                    break
+                case 'pull':
+                    slider.lastIndexAnimation = [
+                        {left: `0`},
+                        {left: `-${getComputedStyle(slider).width}`}
+                    ]
+                    slider.indexAnimation = [
+                        {left: '0', zIndex: 0},
+                        {left: '0', zIndex: 10}
+                    ]
+                    break
+                case 'push':
+                    slider.lastIndexAnimation = [
+                        {zIndex: 0},
+                        {zIndex: 0}
+                    ]
+                    slider.indexAnimation = [
+                        {left: `${getComputedStyle(slider).width}`, zIndex: 0},
+                        {left: '0', zIndex: 10}
+                    ]
+                    break
+            }
+            slider.runAnimation()
+        }
+    }
+
+    slider.previous = () => {
+        if(slider.animationTimeout == null){
+            slider.lastIndex = slider.index
+            slider.index -= 1
+            if(slider.index < 0) slider.index = slider.sliderItems.length-1
+
+            switch(slider.animation){
+                case 'slide':
+                    slider.lastIndexAnimation = [
+                        {left: `0`},
+                        {left: `${getComputedStyle(slider).width}`}
+                    ]
+                    slider.indexAnimation = [
+                        {left: `-${getComputedStyle(slider).width}`},
+                        {left: '0'}
+                    ]
+                    break
+                case 'fade':
+                    slider.lastIndexAnimation = [
+                        {opacity: '1'},
+                        {opacity: '0'}
+                    ]
+                    slider.indexAnimation = [
+                        {opacity: '0'},
+                        {opacity: '1'}
+                    ]
+                    break
+                case 'pull':
+                    slider.lastIndexAnimation = [
+                        {left: `0`},
+                        {left: `${getComputedStyle(slider).width}`}
+                    ]
+                    slider.indexAnimation = [
+                        {left: '0', zIndex: 0},
+                        {left: '0', zIndex: 10}
+                    ]
+                    break
+                case 'push':
+                    slider.lastIndexAnimation = [
+                        {zIndex: 0},
+                        {zIndex: 0}
+                    ]
+                    slider.indexAnimation = [
+                        {left: `-${getComputedStyle(slider).width}`, zIndex: 0},
+                        {left: '0', zIndex: 10}
+                    ]
+                    break
+            }
+            slider.runAnimation()
+        }
+    }
+
+    slider.runAnimation = () => {
+        slider.sliderItems[slider.lastIndex].animate(
+            slider.lastIndexAnimation ,{
+            duration: slider.animationDuration,
+            easing: slider.animationEasing,
+            fill: 'forwards'
+        })
+
+        slider.sliderItems[slider.index].animate(
+            slider.indexAnimation, {
+            duration: slider.animationDuration,
+            easing: slider.animationEasing,
+            fill: 'forwards'
+        })
+
+        slider.animationTimeout = setTimeout(() => {
+            slider.animationTimeout = null
+        }, slider.animationDuration)
+    }
+
+    //autoplay
+    slider.autoplayDelay = Number(slider.getAttribute('autoplay-delay')) || 2000
+
+    slider.autoplay = () => {
+        slider.autoplayTimeout = setTimeout(() => {
+            slider.next()
+
+            slider.autoplay()
+
+        }, slider.autoplayDelay)
+    }
+    if(slider.hasAttribute('autoplay')) slider.autoplay()
+
+    //click
+    if(slider.hasAttribute('click')) slider.addEventListener('click', () => {
+        slider.next()
+    })
+})
